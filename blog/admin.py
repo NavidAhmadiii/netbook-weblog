af2c997cdd4366e74cf6ef141317eb1825e8e774
Navid <navidahmadi6293@gmail.com>
2023-11-19 03:01:44 +0330

    Writing actions (publishing articles and drafting articles) for the Django management panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,41 @@
+from gettext import ngettext
+
 from django.contrib import admin
+from django.core.checks import messages
+
 from .models import Article, Category
 
 
 # Register your models here.
 
+# write actions for admin panel
+def make_published(modeladmin, request, queryset):
+    row_updated = queryset.update(status='p')
+    if row_updated == 1:
+        message_bit = "منتشر شد"
+    else:
+        message_bit = "منتشر شد"
+    modeladmin.message_user(request, '{} مقاله {}'.format(row_updated, message_bit))
+
+
+make_published.short_description = "انتشار مقالات انتخاب شده"
+
+
+# write actions for admin panel
+def make_draft(modeladmin, request, queryset):
+    row_updated = queryset.update(status='d')
+    if row_updated == 1:
+        message_bit = "پیش نویس شد"
+    else:
+        message_bit = "پیش نویس شد"
+    modeladmin.message_user(request, '{} مقاله {}'.format(row_updated, message_bit))
+
+
+make_draft.short_description = "پیش نویس شدن مقالات انتخاب شده"
+
+
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('position', 'title', 'parent','slug', 'status')
+    list_display = ('position', 'title', 'parent', 'slug', 'status')
     list_filter = (['status'])
     search_fields = ('title', 'title')
     prepopulated_fields = {'slug': ('title',)}
@@ -20,9 +50,11 @@ class AricleAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description')
     prepopulated_fields = {'slug': ('title',)}
     ordering = ['-status', '-published']
+    actions = [make_published, make_draft]
 
     def category_to_str(self, obj):
         return "، ".join([category.title for category in obj.category_published()])
+
     category_to_str.short_description = "دسته بندی"
 
 
