d4f2468e74374a2f4bf4a491289a343f03d32f81
Navid <navidahmadi6293@gmail.com>
2023-11-19 02:10:45 +0330

    Changing functions based on views with classes based on views

--- blog/views.py
+++ blog/views.py
@@ -4,40 +4,17 @@ from .models import Article, Category
 from django.views.generic import ListView, DetailView
 
 
-# Create your views here.
-# def home(request, page=1):
-#     articles_list = Article.objects.published()
-#     paginator = Paginator(articles_list, 4)
-#     page = request.GET.get('page')
-#     articles = paginator.get_page(page)
-#     context = {
-#         "articles": articles,
-#     }
-#     return render(request, "article_list.html", context)
-
 class ArticleListView(ListView):
     context_object_name = "articles"
     queryset = Article.objects.published()
     paginate_by = 4
 
 
-# def detail(request, slug):
-#     context = {
-#         "article": get_object_or_404(Article.objects.published(), slug=slug)
-#     }
-#     return render(request, "blog/article_detail.html", context)
-
 class ArticleDetailView(DetailView):
     def get_object(self, queryset=None):
         slug = self.kwargs.get('slug')
         return get_object_or_404(Article.objects.published(), slug=slug)
 
-
-# def category(request, slug):
-#     context = {
-#         "category": get_object_or_404(Category, slug=slug, status=True)
-#     }
-#     return render(request, "blog/category_list.html", context)
 
 class CategoryListView(ListView):
     paginate_by = 4
